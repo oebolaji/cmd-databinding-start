02ec63eeb543852fddc3cb1d7d91187ca5b7780a
Bolaji <Obafemi.Bolaji@stanbicibtc.com>
2021-04-21 21:47:38 +0100

    changes

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,14 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}> ();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}> ();
+
   newServerName = '';
   newServerContent = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput.value);
+    console.log('content input', this.serverContentInput);
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+       serverContent: this.serverContentInput.nativeElement.value
+      })
+    //emit the server created event with the data to be passed;
+    // i.e the details(serverName & content) added from the template
+
     // this.serverElements.push({
     //   type: 'server',
     //   name: this.newServerName,
@@ -22,7 +36,15 @@ export class CockpitComponent implements OnInit {
     // });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      // serverName: this.newServerName,
+      serverName: nameInput.value,
+      //  serverContent: this.newServerContent 
+       serverContent: this.serverContentInput.nativeElement.value
+      })
+
+
     // this.serverElements.push({
     //   type: 'blueprint',
     //   name: this.newServerName,
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // encapsulation: ViewEncapsulation.None //Emulated Shadow DOM
 })
 export class ServerElementComponent implements OnInit {
  @Input('srvElement') element: {type:string, name:string, content:string};
 
-  constructor() { }
+  constructor() {
+
+    console.log('constructor called!')
+   }
 
   ngOnInit(): void {
+    // console.log('element content',this.element.content)
+    console.log('ngOnInIt called!')
   }
 
 }
